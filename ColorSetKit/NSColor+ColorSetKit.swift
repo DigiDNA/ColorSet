1f7e3d64b60bf6e38bc51c5339010b8061f3d12c
macmade <macmade@xs-labs.com>
2019-09-27 03:06:59 +0200

    Added a public HSL extension for NSColor in ColorSetKit.

--- ColorSetKit/NSColor+ColorSetKit.swift
+++ ColorSetKit/NSColor+ColorSetKit.swift
@@ -61,4 +61,181 @@ import Cocoa
         
         return pair?.color
     }
+    
+    @objc convenience init( hue: CGFloat, saturation: CGFloat, lightness: CGFloat, alpha: CGFloat )
+    {
+        let rgb = hslToRGB( hue, saturation, lightness )
+        
+        self.init( srgbRed: rgb.0, green: rgb.1, blue: rgb.2, alpha: alpha )
+    }
+    
+    @objc func getHue( _ hue: UnsafeMutablePointer< CGFloat >?, saturation: UnsafeMutablePointer< CGFloat >?, lightness: UnsafeMutablePointer< CGFloat >?, alpha: UnsafeMutablePointer< CGFloat >? )
+    {
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+        
+        self.getRed( &r, green: &g, blue: &b, alpha: &a )
+        
+        let hsl = rgbToHSL( r, g, b )
+        
+        hue?.pointee        = hsl.0
+        saturation?.pointee = hsl.1
+        lightness?.pointee  = hsl.2
+        alpha?.pointee      = a
+    }
+    
+    @objc func byChangingHue( _ h: CGFloat ) -> NSColor
+    {
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+        
+        self.getRed( &r, green: &g, blue: &b, alpha: &a )
+        
+        let hsl = rgbToHSL( r, g, b )
+        
+        return NSColor( hue: h, saturation: hsl.1, lightness: hsl.2, alpha: a )
+    }
+    
+    @objc func byChangingSaturation( _ s: CGFloat ) -> NSColor
+    {
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+        
+        self.getRed( &r, green: &g, blue: &b, alpha: &a )
+        
+        let hsl = rgbToHSL( r, g, b )
+        
+        return NSColor( hue: hsl.0, saturation: s, lightness: hsl.2, alpha: a )
+    }
+    
+    @objc func byChangingLightness( _ l: CGFloat ) -> NSColor
+    {
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+        
+        self.getRed( &r, green: &g, blue: &b, alpha: &a )
+        
+        let hsl = rgbToHSL( r, g, b )
+        
+        return NSColor( hue: hsl.0, saturation: hsl.1, lightness: l, alpha: a )
+    }
+}
+
+fileprivate func hslToRGB( _ h: CGFloat, _ s: CGFloat, _ l: CGFloat ) -> ( CGFloat, CGFloat, CGFloat )
+{
+    var t1:  CGFloat     = 0.0
+    var t2:  CGFloat     = 0.0
+    var rgb: [ CGFloat ] = []
+    
+    if s == 0.0
+    {
+        return ( l, l, l )
+    }
+    
+    if l < 0.5
+    {
+        t2 = l * ( 1.0 + s )
+    }
+    else
+    {
+        t2 = l + s - l * s
+    }
+    
+    t1 = 2.0 * l - t2
+    
+    rgb.append( h + 1.0 / 3.0 )
+    rgb.append( h )
+    rgb.append( h - 1.0 / 3.0 )
+  
+    for i in 0 ..< 3
+    {
+        if rgb[ i ] < 0.0
+        {
+            rgb[ i ] += 1.0
+        }
+        else if rgb[ i ] > 1.0
+        {
+            rgb[ i ] -= 1.0
+        }   
+        
+        if rgb[ i ] * 6.0 < 1.0
+        {
+            rgb[ i ] = t1 + ( t2 - t1 ) * 6.0 * rgb[ i ]
+        }
+        else if rgb[ i ] * 2.0 < 1.0
+        {
+            rgb[ i ] = t2
+        }
+        else if rgb[ i ] * 3.0 < 2.0
+        {
+            rgb[ i ] = t1 + ( t2 - t1 ) * ( ( 2.0 / 3.0 ) - rgb[ i ] ) * 6.0
+        }
+        else
+        {
+            rgb[ i ] = t1
+        }
+    }
+    
+    return ( rgb[ 0 ], rgb[ 1 ], rgb[ 2 ] )
+}
+
+fileprivate func rgbToHSL( _ r: CGFloat, _ g: CGFloat, _ b: CGFloat) -> ( CGFloat, CGFloat, CGFloat )
+{
+    var h:  CGFloat = 0.0
+    var s:  CGFloat = 0.0
+    var l:  CGFloat = 0.0
+    let v:  CGFloat = max( max( r, g ), b )
+    let m:  CGFloat = min( min( r, g ), b )
+    var vm: CGFloat = 0.0
+    var r2: CGFloat = 0.0
+    var g2: CGFloat = 0.0
+    var b2: CGFloat = 0.0
+    
+    l = ( m + v ) / 2.0
+    
+    if l <= 0.0
+    {
+        return ( h, s, l )
+    }
+    
+    vm = v - m
+    s  = vm
+    
+    if s > 0.0
+    {
+        s = s / ( ( l <= 0.5 ) ? v + m : 2.0 - v - m )
+    }
+    else
+    {
+        return ( h, s, l )
+    }
+    
+    r2 = ( v - r ) / vm
+    g2 = ( v - g ) / vm
+    b2 = ( v - b ) / vm
+  
+    if r == v
+    {
+        h = ( g == m ) ? 5.0 + b2 : 1.0 - g2
+    }
+    else if g == v
+    {
+        h = ( b == m ) ? 1.0 + r2 : 3.0 - b2
+    }
+    else
+    {
+        h = ( r == m ) ? 3.0 + g2 : 5.0 - r2
+    }
+    
+    h /= 6.0
+    
+    return ( h, s, l )
 }
